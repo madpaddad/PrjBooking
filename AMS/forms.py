0999c08da26e0a926dec5df1df3fd3daa0d6e604
madpaddad <Daraphal39@gmail.com>
2024-07-04 07:14:33 +0700

    Creating a form

---
+++ AMS/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import formBook
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = formBook
+        fields = ['firstname', 'lastname', 's_id', 'room', 'time_in', 'time_out']
